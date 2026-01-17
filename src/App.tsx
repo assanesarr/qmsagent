b5789a47b9984e577ee59300026fd36144ceb8c3
Assane Sarr <assanesarr030@gmail.com>
2026-01-17 12:44:42 +0000

    add config app

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,15 @@ import {
 } from "@/components/ui/card"
 
 import { io } from 'socket.io-client';
+import { useLocalStorage } from './hook/local_storage';
+
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButton,
+  InputGroupInput,
+  InputGroupText,
+} from "@/components/ui/input-group"
 
 type CALLED = {
   id: number,
@@ -35,7 +44,8 @@ export const socket = io(import.meta.env.VITE_API_URL);
 
 function App() {
   const [called, setCalled] = useState<CALLED>()
-  const [config, setConfig] = useState(null)
+  const [config, setConfig] = useLocalStorage('config', { guichet: 0 });
+  const [value, setValue] = useState<string>('');
 
   // const socket = io('http://localhost:3000');
   const speak = (g: number) => {
@@ -56,11 +66,41 @@ function App() {
     };
   }, [])
 
+  if (!config || config.guichet === 0) {
+    return (
+      <div className="flex min-h-svh flex-col items-center justify-center  p-4">
+        <Card className="mb-4 min-w-xs ">
+          <CardContent className="flex flex-col items-center gap-4">
+            <CardTitle className="text-2xl font-bold mb-2">Configuration manquante</CardTitle>
+            <p className='text-sm '>Veuillez configurer l'application avant de l'utiliser.</p>
+
+            <InputGroup>
+              <InputGroupAddon>
+                <InputGroupText>
+                  Guichet Numero
+                </InputGroupText>
+              </InputGroupAddon>
+              <InputGroupInput onChange={(e) => {
+                setValue(e.target.value)
+              }} />
+
+              <InputGroupButton onClick={() => {
+                setConfig({ guichet: parseInt(value) })
+              }}>
+                Sauvegarder
+              </InputGroupButton>
+            </InputGroup>
+          </CardContent>
+        </Card>
+      </div>
+    )
+  }
+
   return (
     <div className="flex min-h-svh flex-col items-center justify-center  p-4">
-      <Card className="mb-4 min-w-xs " onClick={() => speak(1)}>
+      <Card className="mb-4 min-w-xs " onClick={() => speak(config?.guichet)}>
         <CardContent className="flex flex-col items-center gap-4">
-          <CardTitle className="text-2xl font-bold mb-2">Guichet 1</CardTitle>
+          <CardTitle className="text-2xl font-bold mb-2">Guichet {config?.guichet}  </CardTitle>
           <h1 className="text-6xl font-bold">
             {called && called?.id}
           </h1>
